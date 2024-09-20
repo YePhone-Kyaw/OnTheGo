e25cab0b69a8342addfbd3ff868376e57e398698
Adwaith Ajith <adwaith.shiniajith@edu.sait.ca>
2024-09-20 03:47:22 -0600

    Home Page 19/09

--- app/_components/nav-bar.js
+++ app/_components/nav-bar.js
@@ -13,8 +13,6 @@ export default function NavBar() {
   return (
     <>
       <div className="flex justify-between bg-black text-black px-10 py-10 w-full">
-        <button>
-          <p className="text-white">On The Go Logo</p>
       <Link href="/pages/home">
         <p className="text-white">On The Go Logo</p>
       </Link>
@@ -30,22 +28,6 @@ export default function NavBar() {
             <path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z" />
           </svg>
         </button>
-        <div className="flex space-x-10">
-          <button className="bg-white px-10 py-5 rounded-md">
-            Get A Quote
-          </button>
-          <button>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 448 512"
-              width={50}
-              height={50}
-              className="fill-current bg-white p-1 rounded-md"
-            >
-              <path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z" />
-            </svg>
-          </button>
-        </div>
         {isOpen && (
           <div className="absolute right-0 mt-2 w-60 bg-white rounded-md shadow-lg py-1 z-20">
             <Link href="/pages/home" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">HOME</Link>
