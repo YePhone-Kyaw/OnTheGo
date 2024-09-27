b5a7ccec3d613b91134d23ded422fa528e4a3b7c
Adwaith Ajith <adwaith.shiniajith@edu.sait.ca>
2024-09-26 23:20:36 -0600

    Video component updates

--- app/_components/video-section.js
+++ app/_components/video-section.js
@@ -1,44 +1,58 @@
 "use client";
 
-export default function TypeOfVideos({type, src}) {
+import { useState } from "react";
+
+export default function TypeOfVideos({ title, src }) {
+
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const controlExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
-    <div className=" flex flex-col items-center space-y-2 h-72"> 
-        <video
-            className=" w-96 h-full object-cover" 
-            controls
-            muted
-            src={src}
-        />
-      <p className="text-center text-sm font-semibold">{type}</p> 
+    <div
+      className={`flex flex-col items-center space-y-2 h-72 ${
+        isExpanded ? 'fixed inset-0 z-50 m-5' : 'relative'}`}>
+      <video
+        className={`object-cover cursor-pointer ${isExpanded ? 'w-4/5 h-auto' : 'w-96 h-full cursor-pointer'}`}
+        controls
+        muted
+        onClick={controlExpand}
+        src={src}
+      />
+      {!isExpanded && (
+        <p className="text-center text-sm font-semibold">{title}</p>
+      )}
     </div>
   );
 }
 
-export function SectorOfExperience({title, src}){
-    return(
-        <div className=" flex flex-col items-center space-y-2 h-72"> 
-        <video
-            className=" w-96 h-full object-cover" 
-            controls
-            muted
-            src={src}
-        />
-      <p className="text-center text-sm font-semibold">{title}</p> 
-    </div>
-    )
-}
+
 
 export function ExamplesOfWork({title,description, src}){
+
+  const [isExpanded, setIsExpanded] = useState(false)
+
+  const controlExpand = () =>{
+    setIsExpanded(!isExpanded)
+  }
+
     return(
-        <div className=" flex flex-col items-center space-y-2 h-72">
+        <div className={`flex flex-col items-center space-y-2 h-72 cursor-pointer ${isExpanded ? 'fixed inset-0 z-50 m-5 cursor-pointer' : 'relative'}` }>
             <video
-            className=" w-96 h-full object-cover"
+            className={`object-cover ${isExpanded ? 'w-4/5 h-auto' : 'w-96 h-full'}`}
             controls
             muted
             src={src}
+            onClick={controlExpand}
             />
-            <p className="text-center text-sm font-semibold">{title}</p>
-            <p className=" text-center text-sm">{description}</p>
+            {!isExpanded && (
+          <p className="text-center text-sm font-semibold">{title}</p>
+            )}
+            {!isExpanded && (
+               <p className=" text-center text-sm">{description}</p>
+            )}
         </div>
     )
 }
